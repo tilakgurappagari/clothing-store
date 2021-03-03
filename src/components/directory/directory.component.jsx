344ccb89361e636fa98458ed6a8040fdef59c4bd
tilakgurappagari <tilak.gurappagari@gmail.com>
2021-03-03 12:44:20 -0600

    started routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,7 +12,7 @@ class Directory extends Component{
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
-                linkUrl: 'shop/hats'
+                linkUrl: 'hats'
               },
               {
                 title: 'jackets',
@@ -46,7 +46,9 @@ class Directory extends Component{
         let sections = this.state.sections.map(section =>(
             <MenuItem title={section.title}
              key={section.id} imageUrl={section.imageUrl}
-             size={section.size}/>
+             size={section.size}
+             linkUrl={section.linkUrl}
+             />
         ))
 
         return(
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss'
 
 const MenuItem =(props)=>{
     return(
-        <div className= {`${props.size} menu-item`}>
+        <div className= {`${props.size} menu-item`}
+            onClick={()=>props.history.push(`${props.match.url}${props.linkUrl}`)}
+        >
             
             <div className="background-image"
             style = {{
@@ -17,4 +20,4 @@ const MenuItem =(props)=>{
     );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
